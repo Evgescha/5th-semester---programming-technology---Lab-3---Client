3a5a5f0f99c62bc62f0034e37ed2aa35c5235b8c
Home <Home@PC>
2018-09-15 15:25:17 +0300

    Создание отдельного проекта для клиента

---
+++ src/Client.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.*;
+
+public class Client {
+	public static void main(String[] args) throws IOException {
+
+		String addres = "192.168.173.84";
+		System.out.println("Welcome to Client side");
+
+		Socket fromserver = null;
+
+		System.out.println("Connecting to... " + addres);
+
+		fromserver = new Socket(addres, 8080);
+		BufferedReader in = new BufferedReader(new InputStreamReader(fromserver.getInputStream()));
+		PrintWriter out = new PrintWriter(fromserver.getOutputStream(), true);
+		BufferedReader inu = new BufferedReader(new InputStreamReader(System.in));
+
+		String fuser, fserver;
+
+		while ((fuser = inu.readLine()) != null) {
+			out.println(fuser);
+			fserver = in.readLine();
+			System.out.println(fserver);
+			if (fuser.equalsIgnoreCase("close"))
+				break;
+			if (fuser.equalsIgnoreCase("exit"))
+				break;
+		}
+
+		out.close();
+		in.close();
+		inu.close();
+		fromserver.close();
+	}
+}
